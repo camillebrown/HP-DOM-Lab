a90ed35b3a525f62b03f89f84db35cdf420dca23
“camillebrown” <camillebrown11@gmail.com>
2020-09-08 16:07:18 -0700

    Year 1 - added h1 using javascript

--- app.js
+++ app.js
@@ -2,4 +2,12 @@
 //     console.log('DOM is loaded!')
 // })
 
-let container = document.querySelector
+let container = document.getElementById('container')
+console.log(container)
+
+let h1 = document.createElement('h1')
+console.log(h1)
+
+h1.innerText = 'Hogwarts'
+
+container.appendChild(h1)
