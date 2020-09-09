3a969076fcc1827e4ab0434e92fa8aa7a53b3b75
“camillebrown” <camillebrown11@gmail.com>
2020-09-08 18:01:08 -0700

    Year 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- app.js
+++ app.js
@@ -54,6 +54,107 @@ function addElements() {
 
 addElements();
 
+let h5 = document.createElement('h5')
+h5.innerText = 'Spring 2017'
+container.appendChild(h5)
+
+let table = document.createElement('table')
+container.appendChild(table)
+
+let thead = document.createElement('thead')
+table.appendChild(thead)
+
+let th1 = document.createElement('th')
+th1.innerText = 'Day'
+thead.appendChild(th1)
+
+let th2 = document.createElement('th')
+th2.innerText = 'Classes'
+thead.appendChild(th2)
+
+//////////////////////////////////////
+
+let tr1 = document.createElement('tr')
+table.appendChild(tr1)
+
+let td1 = document.createElement('td')
+td1.innerText = 'Monday'
+tr1.appendChild(td1)
+
+let td2 = document.createElement('td')
+td2.innerText = 'Herbology & History of Magic'
+tr1.appendChild(td2)
+
+//////////////////////////////////////
+
+let tr2 = document.createElement('tr')
+table.appendChild(tr2)
+
+let td3 = document.createElement('td')
+td3.innerText = 'Tuesday'
+tr2.appendChild(td3)
+
+let td4 = document.createElement('td')
+td4.innerText = 'Quidditch practice & Transfiguration'
+tr2.appendChild(td4)
+
+//////////////////////////////////////
+
+let tr3 = document.createElement('tr')
+table.appendChild(tr3)
+
+let td5 = document.createElement('td')
+td5.innerText = 'Wednesday'
+tr3.appendChild(td5)
+
+let td6 = document.createElement('td')
+td6.innerText = 'Herbology & History of Magic'
+tr3.appendChild(td6)
+
+//////////////////////////////////////
+
+let tr4 = document.createElement('tr')
+table.appendChild(tr4)
+
+let td7 = document.createElement('td')
+td7.innerText = 'Thursday'
+tr4.appendChild(td7)
+
+let td8 = document.createElement('td')
+td8.innerText = 'Quidditch practice & Transfiguration'
+tr4.appendChild(td8)
+
+//////////////////////////////////////
+
+let tr5 = document.createElement('tr')
+table.appendChild(tr5)
+
+let td9 = document.createElement('td')
+td9.innerText = 'Friday'
+tr5.appendChild(td9)
+
+let td10 = document.createElement('td')
+td10.innerText = 'Defense Against the Dark Arts & Potions'
+tr5.appendChild(td10)
+
+
+// let tds = ['Monday', 'Herbology & History of Magic', 'Tuesday', 'Quidditch practice & Transfiguration', 'Wednesday', 'Herbology & History of Magic', 'Thursday', 'Quidditch practice & Transfiguration', 'Friday','Defense Against the Dark Arts & Potions']
+// let trs = [1,2,3,4,5]
+// let tableRows = () => {
+//     for(let i=0; i<trs.length; i++){
+//         var tr = document.createElement('tr')
+//         tr.innerText = 'tr' + trs[i]
+//         table.appendChild(tr)
+//     }
+//     for(let i=0; i<tds.length;i++) {
+//         var td = document.createElement('td');
+//         td.innerText = tds[i];
+//         tr.appendChild(td)
+//     }
+// }
+
+// tableRows();
+
 
 
 
